bb56b4d5330c13db1f96ebaf65c0dcf1469573c4
2324936352 <58315004+2324936352@users.noreply.github.com>
2020-06-23 11:08:00 +0800

    3

--- Sources/LPFW.WebApplication/Areas/MusicUI/Controllers/HomeController.cs
+++ Sources/LPFW.WebApplication/Areas/MusicUI/Controllers/HomeController.cs
@@ -105,7 +105,7 @@ namespace LPFW.WebApplication.Areas.MusicUI.Controllers
                     lyricPath = LyricFileName,
                     PhotoPath = uniqueFileName
                 };
-                StreamWriter sw = new StreamWriter("wwwroot/Music/music/musicList.txt", true, System.Text.Encoding.Default);    //一定在写绝对路径
+                StreamWriter sw = new StreamWriter("wwwroot/Music/music/musicList.txt", true, System.Text.Encoding.Default);    //歌单路径
                 sw.WriteLine($"[{model.Name},{model.SingerName}]"); 
                 sw.Close();//关闭文件
 
@@ -116,77 +116,7 @@ namespace LPFW.WebApplication.Areas.MusicUI.Controllers
             return View();
         }
       
-#region 上传文件
-//private readonly string _targetFilePath = "C:\\Users\\Administrator\\Desktop\\项目\\ChikenCuntS\\Sources\\LPFW.WebApplication\\wwwroot\\Music\\music\\songImg";
-///// <summary>
-///// 流式文件上传
-///// </summary>
-///// <returns></returns>
-//[HttpPost("Create")]
-//public async Task<IActionResult> UploadingStream()
-//{
 
-//    //获取boundary
-//    var boundary = HeaderUtilities.RemoveQuotes(MediaTypeHeaderValue.Parse(Request.ContentType).Boundary).Value;
-//    //得到reader
-//    var reader = new MultipartReader(boundary, HttpContext.Request.Body);
-//    //{ BodyLengthLimit = 2000 };//
-//    var section = await reader.ReadNextSectionAsync();
-
-//    //读取section
-//    while (section != null)
-//    {
-//        var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition, out var contentDisposition);
-//        if (hasContentDispositionHeader)
-//        {
-//            var trustedFileNameForFileStorage = Path.GetRandomFileName();
-//            await WriteFileAsync(section.Body, Path.Combine(_targetFilePath, trustedFileNameForFileStorage));
-//        }
-//        section = await reader.ReadNextSectionAsync();
-//    }
-//    return Created(nameof(HomeController), null);
-//}
-
-///// <summary>
-///// 缓存式文件上传
-///// </summary>
-///// <param name=""></param>
-///// <returns></returns>
-//[HttpPost("UploadingFormFile")]
-//public async Task<IActionResult> UploadingFormFile(IFormFile file)
-//{
-//    using (var stream = file.OpenReadStream())
-//    {
-//        var trustedFileNameForFileStorage = Path.GetRandomFileName();
-//        await WriteFileAsync(stream, Path.Combine(_targetFilePath, trustedFileNameForFileStorage));
-//    }
-//    return Created(nameof(HomeController), null);
-//}
-
-
-///// <summary>
-///// 写文件导到磁盘
-///// </summary>
-///// <param name="stream">流</param>
-///// <param name="path">文件保存路径</param>
-///// <returns></returns>
-//public static async Task<int> WriteFileAsync(System.IO.Stream stream, string path)
-//{
-//    const int FILE_WRITE_SIZE = 84975;//写出缓冲区大小
-//    int writeCount = 0;
-//    using (FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write, FILE_WRITE_SIZE, true))
-//    {
-//        byte[] byteArr = new byte[FILE_WRITE_SIZE];
-//        int readCount = 0;
-//        while ((readCount = await stream.ReadAsync(byteArr, 0, byteArr.Length)) > 0)
-//        {
-//            await fileStream.WriteAsync(byteArr, 0, readCount);
-//            writeCount += readCount;
-//        }
-//    }
-//    return writeCount;
-//} 
-#endregion
 
 
 /// <summary>
--- Sources/LPFW.WebApplication/wwwroot/Music/js/music.js
+++ Sources/LPFW.WebApplication/wwwroot/Music/js/music.js
@@ -597,7 +597,7 @@ function loadFile(name) {
     let xhr = new XMLHttpRequest(),
         okStatus = document.location.protocol === "file:" ? 0 : 200;
     xhr.open('GET', name, false);
-    xhr.overrideMimeType("text/html;charset=gbk");
+    xhr.overrideMimeType("text/html;charset=utf-8");
     xhr.send(null);
     return xhr.responseText;
     //return xhr.status === okStatus ? xhr.responseText : null;
